f7093ed4e476f0c36c3b71e5a3b8c24f368f0bbd
knightdark10 <79816266+knightdark10@users.noreply.github.com>
2022-01-27 18:22:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,90 +1,18 @@
-import logging
-import socket
-import time
-import os
-from threading import *
+#!/usr/bin/python
 
-from Logic.Device import Device
-from Logic.Player import Players
-from Packets.Factory import packets
+import os, time
 
-logging.basicConfig(filename="errors.log", level=logging.INFO, filemode="w")
+for iter in xrange(5):
 
+    adb_reboot = os.popen("sudo adb reboot")
+    print(adb_reboot)
+    print('[INFO] Starting Server...')
+    time.sleep(15)
 
-def _(*args):
-	print('[DEBUG]', end=' ')
-	for arg in args:
-		print(arg, end=' ')
-	print()
+    adb_wait = os.popen("sudo adb wait-for-device")
+    print(adb_wait)
+    time.sleep(15)
+    print('[INFO] Server Started! Lets Play Clash of Clans.')
 
-
-class Server:
-	def __init__(self, ip: str, port: int):
-		self.server = socket.socket()
-		self.port = port
-		self.ip = ip
-
-	def start(self):
-		self.server.bind((self.ip, self.port))
-		_(f'Server started! Ip: {self.ip}, Port: {self.port}')
-		while True:
-			self.server.listen()
-			client, address = self.server.accept()
-			_(f'New connection! Ip: {address[0]}')
-			ClientThread(client, address).start()
-
-
-class ClientThread(Thread):
-	def __init__(self, client, address):
-		super().__init__()
-		self.client = client
-		self.address = address
-		self.device = Device(self.client)
-		self.player = Players(self.device)
-
-	def recvall(self, length: int):
-		data = b''
-		while len(data) < length:
-			s = self.client.recv(length)
-			if not s:
-				print("Error Receiving data!")
-				break
-			data += s
-		return data
-
-	def run(self):
-		last_packet = time.time()
-		try:
-			while True:
-				header = self.client.recv(7)
-				if len(header) > 0:
-					last_packet = time.time()
-					packet_id = int.from_bytes(header[:2], 'big')
-					length = int.from_bytes(header[2:5], 'big')
-					data = self.recvall(length)
-
-					if packet_id in packets:
-						_(f'Received packet! Id: {packet_id}')
-						message = packets[packet_id](self.client, self.player, data)
-						message.decode()
-						message.process()
-					else:
-						_(f'Packet don\'t handled! Id: {packet_id}')
-				if time.time() - last_packet > 10:
-					print(f"[INFO] Ip: {self.address[0]} disconnected!")
-					self.client.close()
-					break
-		except ConnectionAbortedError:
-			print(f"[DEBUG] Ip: {self.address[0]} disconnected!")
-			self.client.close()
-		except ConnectionResetError:
-			print(f"[DEBUG] Ip: {self.address[0]} disconnected!")
-			self.client.close()
-		except TimeoutError:
-			print(f"[DEBUG] Ip: {self.address[0]} disconnected!")
-			self.client.close()
-
-
-if __name__ == '__main__':
-	server = Server('0.0.0.0', 9339)
-	server.start()
+    adb_device = os.popen("sudo adb devices -l")
+    print(adb_device)
